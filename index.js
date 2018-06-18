f4d172456205b907abefa1f2686ed7266257bf23
Yaron Thurm <yaron@gigya-inc.com>
2018-06-18 12:29:49 +0300

    Added hard coded users to usres table

--- index.js
+++ index.js
@@ -2,9 +2,27 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+var users = [
+	{
+		uid: 'uid1',
+		email: 'email1@sap.com',
+		isConsentGranted: true,
+		consentLastModified: '2018-06-15T10:54:58.473Z',
+		docVersion: 1.0		
+	},
+	{
+		uid: 'uid2',
+		email: 'email2@sap.com',
+		isConsentGranted: false,
+		consentLastModified: '2018-06-15T10:54:58.473Z',
+		docVersion: 1.0		
+	}
+]
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/Test', (req, res) => res.render('pages/Test', {users:users}))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
