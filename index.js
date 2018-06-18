83a9cee134d4b671ee5937569493e69db93fc83c
Yaron Thurm <yaron@gigya-inc.com>
2018-06-18 15:00:00 +0300

    Clean up

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/Test', (req, res) => renderConsentUsers(req, res))
+  .get('/consent-users', (req, res) => renderConsentUsers(req, res))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
   
 
@@ -16,25 +16,38 @@ express()
   
 function renderConsentUsers(req, res){
 	
-	var apikey = encodeURIComponent('3_pdjqlYp0QLnwmeMbuSUBcf99IbPd6_DfjYgkgZ6wRmsDR3DAM2-UQOJazI3vPs2F');
+	var apikey = encodeURIComponent('3_HJZUjKpjd2FCO-hxLCDiOAVlFsznc7XilpDP-Z4AR_ifNRhB3MZUOP0chnwoIWoG');
 	var userKey = encodeURIComponent(process.env.userKey);
 	var userSecret = encodeURIComponent(process.env.userSecret);
 	var query = encodeURIComponent('select UID,preferences from accounts limit 30');
 	var url = 'https://accounts.eu1.gigya.com/accounts.search?apikey='+apikey+'&userKey='+userKey+'&secret='+userSecret+'&query='+query;
 	
-	var getConsentObject = x => x.preferences.privacy.WebIDE_Privacy_Demo_1;
+	var pathToConsent = '';
+	if (req.query.policy === '1')
+		pathToConsent = 'WebIDE_Privacy_Demo_1'
+	else if (req.query.policy === '2')
+		pathToConsent = 'WebIDE_Demo_2'
+	else if (req.query.policy === '3')
+		pathToConsent = 'WebIDE_Demo_3'
+	else {
+		res.render('pages/Error', {error:'Invalid value for policy param. Supported values are 1, 2, 3'});
+		return;
+	}
+	var getConsentObject = x => x.preferences.privacy[pathToConsent];
+	
 	
 	outboundRequest(url, (error, inboundResponse, body) => {
 		if (error || inboundResponse.statusCode != 200) {
-			// TODO - Handle error
 			console.log('Got error calling accounts.search');
+			res.render('pages/Error', {error:'Failed calling Gigya (accounts.search)', details: body});
 			return;
 		}
 		
 		var json = JSON.parse(body);
 		if (json.errorCode != 0){
-			console.log('accounts.search return failure error code');
+			console.log('accounts.search return failure error code');			
 			console.log(body);
+			res.render('pages/Error', {error:'Got error from Gigya (accounts.search)', details: body });
 			return;
 		}
 		
@@ -43,14 +56,17 @@ function renderConsentUsers(req, res){
 			ret.uid = x.UID;
 			ret.email = '';
 			if (x.preferences !== undefined){
-				ret.isConsentGranted = getConsentObject(x).isConsentGranted;
-				ret.consentLastModified = getConsentObject(x).lastConsentModified;
-				ret.docVersion = getConsentObject(x).docVersion;
+				var consentObject = getConsentObject(x);
+				if (consentObject !== undefined) {
+					ret.isConsentGranted = getConsentObject(x).isConsentGranted;
+					ret.consentLastModified = getConsentObject(x).lastConsentModified;
+					ret.docVersion = getConsentObject(x).docVersion;
+				}
 			}
 			return ret;
 		});
 		
-		res.render('pages/Test', {users:users})		
+		res.render('pages/ConsentUsers', {users:users})		
 	});
 };
 
